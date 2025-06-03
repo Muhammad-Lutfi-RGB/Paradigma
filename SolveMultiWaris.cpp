d140234b0fb5348d2ad9cef391817742ff19984a
Muhammad-Lutfi-RGB <muhammad.lutfi.ft24@mail.umy.ac.id>
2025-06-03 18:39:34 +0700

    membuat class pelajar turunan dari class orang serta memperbaiki beberapa kesalahan

--- SolveMultiWaris.cpp
+++ SolveMultiWaris.cpp
@@ -9,9 +9,16 @@ class orang {
     }
 };
 
-class pekerja : virtual public orang {
+class pekerja : public orang {
     public:
     pekerja(int pUmur) : orang (pUmur) {
         cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : public orang {
+    public:
+    pelajar(int pUmur) : orang (pUmur) {
+        cout << "pelajar dibuat\n" << endl;
     }
 };
