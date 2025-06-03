50a3a9d44f411cb039a268d833754f653cd17942
Muhammad-Lutfi-RGB <muhammad.lutfi.ft24@mail.umy.ac.id>
2025-06-03 18:35:13 +0700

    membuat class orang di file solve multi waris

---
+++ SolveMultiWaris.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+    public:
+    int umur;
+    orang(int pUmur) : umur (pUmur) {
+        cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+    }
+};
